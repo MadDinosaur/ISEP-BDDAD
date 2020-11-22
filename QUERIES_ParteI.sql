68ef91a31d8f1cfa085a60e97cc422207795e510
Barbara Pinto <bdianapinto@outlook.com>
2020-11-22 20:41:28 +0000

    Correção da 2.b).

--- QUERIES_ParteI.sql
+++ QUERIES_ParteI.sql
@@ -18,8 +18,9 @@ AND EXTRACT(year FROM dataReserva) = EXTRACT(year FROM SYSDATE)
 AND c.NIF NOT IN (SELECT clienteNIF FROM reserva WHERE tipoQuarto LIKE 'suite' AND dataReserva = r.dataReserva)
 AND dataReserva = r.dataReserva
 ORDER BY 2,1;
---Parte II
 
+
+--Parte II
 -- (a)
 
 select nome, localidade, concelho
@@ -73,8 +74,8 @@ from reserva r;
 
 -- (b)
 
-/*
 
+/*
 -- selecionar as limpezas dos últimos 6 meses
 select *
 from limpeza l
@@ -82,18 +83,18 @@ where months_between(current_timestamp, l.data)<=6;
 
 --selecionar a média de estadia em cada quarto
 
-select avg(r.dataSaida - r.dataEntrada)
+select tipoQuarto, avg(r.dataSaida - r.dataEntrada)
 from reserva r
 group by tipoQuarto;
 
 -- selecionar os quartos com mais tempo de estadia do que a média
 
-select numeroAndar, numeroSequencial
+select DISTINCT numeroAndar, numeroSequencial
 from reserva r1
 where (r1.dataSaida-r1.dataEntrada) > (select avg(r2.dataSaida - r2.dataEntrada)
-from reserva r2
-group by tipoQuarto
-having r1.tipoQuarto=r2.tipoQuarto);
+                                        from reserva r2
+                                        group by tipoQuarto
+                                        having r1.tipoQuarto=r2.tipoQuarto);
 
 -- selecionar nome camareira
 
@@ -101,53 +102,39 @@ select nome
 from funcionario f, funcionarioCamareira fc
 where fc.funcionarioNIF in f.nif;
 
-*/
+-- selecionar camareira com mais intervencoes
 
+select *
+FROM limpeza
+WHERE funcionarionif = (select funcionarionif from 
+                            (select fc.funcionarionif, count(l.numeroSequencial)
+                            from funcionarioCamareira fc inner join limpeza l on fc.funcionarioNIF = l.funcionarionif
+                            group by fc.funcionarionif
+                            order by 2 desc, 1) 
+                        where rownum = 1);
+*/                        
 -- solucao
-
 SELECT DISTINCT
     EXTRACT(MONTH FROM(l.data)) AS mês,
-    f.nome
-FROM
-    funcionario f, limpeza l, quarto q, tipoQuarto tq, reserva r, funcionarioCamareira fc
-WHERE
-    fc.funcionarioNIF = f.NIF AND l.funcionarioNIF = f.NIF AND q.numeroSequencial = l.numeroSequencial
-    AND q.numeroAndar = l.numeroAndar   AND tq.tipoQuarto = q.tipoQuarto   AND q.numeroSequencial = r.numeroSequencial
-    AND q.numeroAndar = r.numeroAndar   AND months_between(current_timestamp, l.data)<=6
-    AND l.data < sysdate
-    AND ( SELECT DISTINCT AVG(EXTRACT(DAY FROM(r.dataSaida)) - EXTRACT(DAY FROM(r.dataEntrada)))
-        FROM reserva r, quarto q1, tipoQuarto tq1
-        WHERE q1.numeroSequencial = r.numeroSequencial  AND q1.numeroAndar = r.numeroAndar 
-        AND q1.tipoQuarto = tq1.tipoQuarto) < r.dataSaida - r.dataEntrada
-    AND (SELECT COUNT(l.numeroSequencial)
-        FROM limpeza l1, quarto q1, funcionarioCamareira fc1, funcionario  f1
-        WHERE
-            q1.numeroSequencial = l1.numeroSequencial AND q1.numeroAndar = l1.numeroAndar AND fc1.funcionarioNif = f.NIF
-            AND fc1.funcionarioNif = f1.NIF AND q.numeroSequencial = q1.numeroSequencial AND q1.numeroAndar = q.numeroAndar
-            AND f1.NIF != f.NIF
-    ) < ALL (SELECT COUNT(l.numeroSequencial)
-        FROM limpeza l1, quarto q1, funcionarioCamareira fc1, funcionario f1
-        WHERE
-            q.numeroSequencial = l1.numeroSequencial AND q1.numeroAndar = l1.numeroAndar AND fc.funcionarioNif = f.NIF
-            AND fc.funcionarioNif = f1.NIF AND q.numeroSequencial = q1.numeroSequencial AND q1.numeroAndar = q.numeroAndar
-            AND f1.NIF = f.NIF
-    )
-GROUP BY EXTRACT(MONTH FROM(l.data)), f.nome ORDER BY EXTRACT(MONTH FROM(l.data));
-
-/*
-select nome
-from funcionario f, funcionarioCamareira fc, limpeza l
-where fc.funcionarioNIF in f.nif and fc.funcionarioNIF in l.funcionarioNIF and
-months_between(current_timestamp, l.data)<=6 and (l.numeroAndar, l.numeroSequencial) in (
-select r1.numeroAndar, r1.numeroSequencial
-from reserva r1
-where (r1.dataSaida-r1.dataEntrada) > (select avg(r2.dataSaida - r2.dataEntrada)
-from reserva r2
-group by tipoQuarto
-having r1.tipoQuarto=r2.tipoQuarto));
-*/
-
-
+    f.nome,
+    f.nif
+FROM funcionario f INNER JOIN funcionarioCamareira fc ON f.nif = fc.funcionarionif
+INNER JOIN limpeza l ON fc.funcionarioNIF = l.funcionarioNIF
+WHERE months_between(current_timestamp, l.data)<=6
+AND l.data < sysdate
+AND f.nif = (select funcionarionif from 
+                            (select fc.funcionarionif, count(l.numeroSequencial)
+                            from funcionarioCamareira fc inner join limpeza l on fc.funcionarioNIF = l.funcionarionif
+                            where l.numeroSequencial in (select DISTINCT numeroSequencial
+                                                        from reserva r1
+                                                        where (r1.dataSaida-r1.dataEntrada) > (select avg(r2.dataSaida - r2.dataEntrada)
+                                                                                                from reserva r2
+                                                                                                group by tipoQuarto
+                                                                                                having r1.tipoQuarto=r2.tipoQuarto))
+                            group by fc.funcionarionif
+                            order by 2 desc, 1) 
+                        where rownum = 1);
+                        
 
 --Parte III --
 --a)
