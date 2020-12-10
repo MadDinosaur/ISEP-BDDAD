390fe01900cb1c94b53cded518f27f9eb0b4cc63
Pedro Vieira <1181079@isep.ipp.pt>
2020-12-10 15:37:03 +0000

    Continuação do  exercício 3

--- trgEpocasNaoSobrepostas.sql
+++ trgEpocasNaoSobrepostas.sql
@@ -1,53 +1,57 @@
 CREATE OR REPLACE TRIGGER trgEpocasNaoSobrepostas
 
 BEFORE INSERT OR UPDATE ON EPOCA
-
+REFERENCING NEW AS NEW OLD AS OLD
+FOR EACH ROW
 DECLARE 
 
+/*
     CURSOR cDataInicio IS
         SELECT data_ini
         FROM epoca;
    
     CURSOR cDataFim is
-        SELECT data_ini
+        SELECT data_fim
         FROM epoca;
 
-    vDataInicio cDataInicio%ROWTYPE;
-    vDataFim cDataFim%ROWTYPE;
+*/
+    vDataInicio epoca.data_ini%TYPE;
+    vDataFim epoca.data_fim%TYPE;
     
     dataInicioException EXCEPTION;
     dataFimException EXCEPTION;
     
+    
 BEGIN
 
     LOOP
-        FETCH cDataInicio INTO vDataInicio;
-        FETCH cDataFim INTO vDataFim;
+      --  FETCH cDataInicio INTO vDataInicio;
+      --  FETCH cDataFim INTO vDataFim;
             
-        IF(:new.data_ini>vDataInicio AND :new.data_ini<vDataFim) THEN
+        IF(:new.data_ini between vDataInicio AND vDataFim) THEN
     
             raise dataInicioException;
     
         END IF;
     
-        IF(:new.data_fim>vDataInicio AND :new.data_fim<vDataFim) THEN
+        IF(:new.data_fim between vDataInicio AND vDataFim) THEN
     
             raise dataFimException;
     
         END IF;
     END LOOP;
-END;
 
 EXCEPTION
     WHEN dataInicioException THEN
         RAISE_APPLICATION_ERROR(-20001, 'Data Início Inválida');
-        RETURN null;
         
     WHEN dataFimException THEN
         raise_application_error(-20002, 'Data Fim Inválida.');
-        RETURN null;
 END;
 /
+
+alter trigger trgEpocasNaoSobrepostas enable;
+set SERVEROUTPUT on;
     
-    
+insert into epoca(id, nome, data_ini, data_fim) values(5, 'Época 2', to_date('2020-04-03', 'yyyy-mm-dd'), to_date('2020-06-30', 'yyyy-mm-dd'));
     
