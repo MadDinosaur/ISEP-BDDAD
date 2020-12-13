3191763f4001792836e2bc36c14773448dc6f683
Pedro Vieira <1181079@isep.ipp.pt>
2020-12-13 16:12:51 +0000

    Exercício 1 - Continuação

--- fncGetQuartoReserva.sqp.sql
+++ fncGetQuartoReserva.sqp.sql
@@ -1,4 +1,4 @@
-create or replace function fncGetQuartoReseva(vReserva reserva.id%type) return 
+create or replace function fncGetQuartoReseva(pReserva reserva.id%type) return 
 quarto.id%type
 
 is
@@ -12,6 +12,7 @@ is
     idReservaDoesntExist exception;
     reservasAlreadyHasARoom exception;
     reservaIsNotOpened exception;
+    theresNoAvailableRoom exception;
 
 begin
 
@@ -53,7 +54,31 @@ begin
     from reserva r
     where r.id = vReserva;
 
-
+-- obtem o quarto que respeita as condições
+-- falta limitar a soma dos dias ao ultimo ano
+    open cQuartoId for
+        with ocupacao as (
+        select ci.id_quarto, sum(co.data - ci.data - 1) as dias
+        from checkin ci, checkout co
+        where co.id_reserva = ci.id_reserva and ci.data > vDataEntrada - 365
+        group by ci.id_quarto)
+        
+        select q.id
+        from quarto q, andar a, ocupacao 
+        where q.id_andar = a.id and o.id_quarto = q.id
+        order by a.nr_andar, o.dias;
+        
+        loop
+            fetch cQuartoId into vQuartoId;
+            exit when cQuartoId%notfound;
+            if not isQuartoIndisponivel(vQuartoId, vDataEntrada) then
+                return vQuartoId;
+            end if;
+        end loop;
+        
+    close cQuartoId;
+    
+    raise theresnoAvailableRoom;
     
     exception
         when idReservaIsNull then
@@ -67,4 +92,8 @@ begin
             
         when reservaIsNotOpened then
             return null;
+            
+        when theresNoAvailableRoom then
+            return null;
+            
 end;
