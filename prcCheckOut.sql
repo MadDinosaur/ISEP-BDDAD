13788829ddd361a2e749c7c11457dfe57febf046
Pedro Vieira <1181079@isep.ipp.pt>
2020-12-15 17:55:14 +0000

    Exercício 2 concluído

--- prcCheckOut.sql
+++ prcCheckOut.sql
@@ -7,6 +7,7 @@ AS
     vPrecoReserva fatura.valor_faturado_reserva%TYPE;
     vPrecoConsumo fatura.valor_faturado_consumo%TYPE;
     vReservaHasCheckout reserva.id%TYPE;
+    vFatura fatura.id%TYPE;
     
     vIdMaximo fatura.id%TYPE;
     vNumeroMaximo fatura.id%TYPE;
@@ -49,14 +50,7 @@ BEGIN
     SELECT SUM(lcc.preco_unitario * lcc.quantidade) INTO vPrecoConsumo
     FROM linha_conta_consumo lcc
     WHERE lcc.id_conta_consumo = (SELECT id FROM conta_consumo WHERE id_reserva = pReserva.id);
-   
-    SELECT MAX(id)+1 INTO vIdMaximo
-    FROM fatura;
-   
-    IF vIdMaximo=null THEN
-    vIdMaximo:=1;
-    END IF;
-   
+    
     SELECT MAX(numero)+1 INTO vNumeroMaximo 
     FROM fatura;
     
@@ -79,11 +73,13 @@ BEGIN
         END LOOP;
     CLOSE cIdCheckout;
     
-    dbms_output.put_line(pReserva.id);
 
+    vFatura := seq_fatura.nextval;
+    
     INSERT INTO checkout(id_reserva, data, valor_extra) VALUES (pReserva.id, vCheckoutDate, vValorExtra);
-    INSERT INTO fatura(id, numero, data, id_cliente, id_reserva, valor_faturado_reserva, valor_faturado_consumo) VALUES (vIdMaximo, vNumeroMaximo, vCheckoutDate, pReserva.id_cliente, pReserva.id, vPrecoReserva, vPrecoConsumo);
-
+    INSERT INTO fatura(id, numero, data, id_cliente, id_reserva, valor_faturado_reserva, valor_faturado_consumo) VALUES (vFatura, vFatura, vCheckoutDate, pReserva.id_cliente, pReserva.id, vPrecoReserva, vPrecoConsumo);
+    UPDATE reserva
+    SET reserva.id_estado_reserva = 5;
     EXCEPTION
         WHEN parametroInvalidoException THEN
             RAISE_APPLICATION_ERROR(-20001, 'Parametro Inválido');
@@ -114,21 +110,33 @@ end;
 DECLARE
 
 vReserva reserva%ROWTYPE;
-
+v_id_reserva reserva.id%TYPE;
+vCheck checkout.id_reserva%TYPE;
 
 Begin
-    BEGIN
+
+    v_id_reserva := seq_reserva.nextval;
     
     INSERT INTO reserva(id, id_cliente, nome, id_tipo_quarto, data, data_entrada, data_saida, nr_pessoas, preco, id_estado_reserva)
-    VALUES (76868, 196, null, 1, '2045-12-5', '2045-12-6', '2045-12-8', 1, 23, 1);
-    SELECT * INTO vReserva
-    from reserva 
-    where id=76868;
+    VALUES (v_id_reserva, 196, null, 1, '2045-12-5', '2045-12-6', '2045-12-8', 1, 23, 1);
     
-       prcCheckOut(vReserva);
-    end;
+    INSERT INTO checkin(id_reserva, data, id_quarto) VALUES(v_id_reserva, '2045-12-6', 19);
+    
+    SELECT * INTO vReserva
+    FROM reserva 
+    WHERE id=v_id_reserva;
+    
+    prcCheckOut(vReserva);
+       
+    SELECT id_reserva INTO vCheck
+    FROM Checkout
+    where id_reserva=v_id_reserva;
+       
+    dbms_output.put_line('id da reserva: ' || vCheck);
+    ROLLBACK;
 end;
 /
+
 
 
 set serveroutput on;
